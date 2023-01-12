cad7ff686c3b7439bc0f3b25e00f281e60a6d65e
C. Sean Burns <sean.csb@gmail.com>
2023-01-12 15:51:55 -0500

    received help from stack overflow users to solve creating a new variable with nation info

--- geomod.py
+++ geomod.py
@@ -1,20 +1,15 @@
 import pandas as pd
 import spacy
+nlp = spacy.load("en_core_web_sm")
 
 titles = pd.read_csv('titles.csv', header = 0, names = ['Citations', 'Titles'], index_col = False)
+titles['Titles'] = titles['Titles'].astype(str).str.lower()
+titles['Nation'] = [s if (s:=''.join([ent.text for ent in nlp(i).ents])) else 'NA'
+                  for i in titles['Titles']]  
 
-nlp = spacy.load("en_core_web_sm")
-   
-# for i in range(len(titles)):
-#   doc = nlp(titles.Titles[i].lower())
-  
-doc = nlp(titles.Titles[2060].lower())
-for ent in doc.ents:
-    print(ent.text)
- 
 ---
-
-# this is it:  -->
+ 
+# this is kind of it but not totally:  -->
 
 # here I get the country names
 data = [[10, 'I live in America'], [20, 'You ate a cookie'], [30, 'He went to Mexico']]
@@ -27,18 +22,28 @@ for i in data['Text']:
 
 # <-- that is it! 
 
-# here I get the country names in a list!
+# Got help from StackOverflow
+# https://stackoverflow.com/questions/75091694/python-create-column-in-pandas-data-frame-that-matches-row-for-row-values-found
 data = [[10, 'I live in America'], [20, 'You ate a cookie'], [30, 'He went to Mexico']]
 data = pd.DataFrame(data, columns = ['Numbers', 'Text'])
 data['Text'] = data['Text'].astype(str).str.lower()
-countries = []
-for i in data['Text']:
-  doc = nlp(i)
-  for ent in doc.ents:
-    countries.append(ent.text)
+data['Nation'] = [s if (s:=''.join([ent.text for ent in nlp(i).ents])) else 'NA'
+                  for i in data['Text']]  
+
+# alternate from stack overflow
+import pycountry
+data = [[10, 'I went to Mexico'], [20, 'He ate a sandwich'], [30, 'I went to Canada']]
+df = pd.DataFrame(data=data, columns=['Numbers', 'Text'])
+countries = [x.name for x in pycountry.countries]
+df["Nation"] = df["Text"].str.split(" ").apply(lambda x: ",".join([i for i in x if i in countries])).replace("", "NA")
+print(df)
     
 #######
 
+doc = nlp(titles.Titles[2060].lower())
+for ent in doc.ents:
+    print(ent.text)
+ 
 ### To do: create a list with Null values where no country is listed
 data = [[10, 'I live in America'], [20, 'You ate a cookie'], [30, 'He went to Georgia']]
 data = pd.DataFrame(data, columns = ['Numbers', 'Text'])
